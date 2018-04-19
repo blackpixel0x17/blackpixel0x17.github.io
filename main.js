6fffb069e4af5d0f726855179bbca8f44fb17053
blackpixel0x17 <sahil23.raut@gmail.com>
2018-04-20 00:29:23 +0530

    new version

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var urls = new Array();
 
 urls[0] = "https://blackpixel0x17.github.io/1000.html";
+urls[1] = "https://blackpixel0x17.github.io/1002.html";
 
 var random = Math.floor(Math.random()*urls.length);
