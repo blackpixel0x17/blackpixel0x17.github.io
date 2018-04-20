b9a629c4e2f14354411a5f582a5a394345253718
blackpixel0x17 <sahil23.raut@gmail.com>
2018-04-20 07:39:17 +0530

    url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var urls = new Array();
 
-urls[0] = "consultingfaf.fun/1000.html";
-urls[1] = "consultingfaf.fun/1002.html";
-urls[2] = "consultingfaf.fun/1003.html";
+urls[0] = "https://blackpixel0x17.github.io/1000.html";
+urls[1] = "https://blackpixel0x17.github.io/1002.html";
+urls[2] = "https://blackpixel0x17.github.io/1003.html";
 
 var random = Math.floor(Math.random()*urls.length);
